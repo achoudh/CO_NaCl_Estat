9c0aff5184198718edded370e955af164a4b8f56
akandra <akandra1@gmail.com>
2023-05-10 15:54:41 +0200

    starting monolayer

--- metropolis.jl
+++ metropolis.jl
@@ -82,8 +82,8 @@ function simulated_annealing(initial_state::Vector{Float64}, rstep::Float64, ast
     return best_state, best_energy, del, int_min
 end
 
-
-function energy(x::Vector{Float64}) ::Float64
+# Single CO on NaCl
+function energy_single(x::Vector{Float64}) ::Float64
 
     rvec::Vector{Float64} = x[1:3] * a0_surf
     theta::Float64 = x[4] * degrees
@@ -100,6 +100,49 @@ function energy(x::Vector{Float64}) ::Float64
     
     return (out_attr+out_rep-out_disp) * joule2wn
 end
+ 
+function energy(x) #all coordinate of the xC_unitcell is set free except z_ml
+
+    theta_ml = x[1+0*nmols_ml:1*nmols_ml] .* pi / 180.0 #fill(38.0,4) * pi / 180.0
+    phi_ml = x[1+1*nmols_ml:2*nmols_ml] .* pi / 180
+    ml_in = zeros(Float64, nmols_ml, 3)
+    ml_in[:,1] = x[1+2*nmols_ml:3*nmols_ml]
+    ml_in[:,2] = x[1+3*nmols_ml:4*nmols_ml]
+    ml_in[:,3] = x[1+4*nmols_ml:5*nmols_ml] #fill(x[17],4)
+    
+    ml_in[:,1] = ml_in[:,1] - round.(ml_in[:,1])
+    ml_in[:,2] = ml_in[:,2] - round.(ml_in[:,2])
+    
+    comml = comml1 + ml_in
+    
+    Vmlml = zeros(Float64,10)
+    for i in 1:nmols_ml-1, j in i+1:nmols_ml
+        local rvec12 = (comml[i,:] - comml[j,:])
+        rvec12[1] = rvec12[1] - 2*nx*round(Int, rvec12[1]/(2*nx))
+        rvec12[2] = rvec12[2] - 2*ny*round(Int, rvec12[2]/(2*ny))
+        rvec12 = a0_surf .* rvec12
+        Vmlml .+= site_site_interaction(rvec12, bondlength_ml[i], phi_ml[i], theta_ml[i],bondlength_ml[j], phi_ml[j], theta_ml[j])
+    end
+    
+    Vmlsurf = zeros(Float64,3)
+    for i in 1:nmols_ml
+        stheta, sphi, costheta, cosphi = sin(theta_ml[i]), sin(phi_ml[i]), cos(theta_ml[i]), cos(phi_ml[i])
+        rvec = ml_in[i,:] .* a0_surf
+        rvec[3] += z_ml_ang
+
+        ml_o = rvec + [v*stheta*cosphi, v*stheta*sphi, v*costheta]
+        ml_c = rvec + [-w*stheta*cosphi, -w*stheta*sphi, -w*costheta]
+        ml_bc = rvec + [-bc*stheta*cosphi, -bc*stheta*sphi, -bc*costheta]
+        
+        out_attr = mol_surf_attr_stone(ml_o, ml_c, ml_bc, costheta)
+        out_rep, out_disp = mol_surf_rep_stone(ml_o, ml_c, 4)
+        Vmlsurf .+= [out_attr, out_rep,- out_disp]
+    end
+
+    out = Vmlml[1] + sum(Vmlsurf)
+
+   return out*joule2wn/nmols_ml
+end 
 
 initial_state::Vector{Float64} = [0.0, 0.0, 3.1e-10/a0_surf, 0.0 , 1.0] 
 
